723fea3387eba0f71b76983bcfd426c339b7d437
Alan Miscala <alan.miscala0211@gmail.com>
2026-07-11 14:50:53 +0800

    Sprint 5.7 Batch 2A.1: Upgrade Cost Code toolbar for standardized import/export actions

--- src/features/masters/cost-code/components/CostCodeToolbar.tsx
+++ src/features/masters/cost-code/components/CostCodeToolbar.tsx
@@ -1,43 +1,67 @@
 import {
-    MasterToolbar,
-  } from "@/components/master-data";
-  
-  interface Props {
-  
-    keyword: string;
-  
-    onKeywordChange(
-      value: string
-    ): void;
-  
-    onCreate(): void;
-  
-  }
-  
-  export default function CostCodeToolbar({
-  
-    keyword,
-  
-    onKeywordChange,
-  
-    onCreate,
-  
-  }: Props) {
-  
-    return (
-  
-      <MasterToolbar
-  
-        keyword={keyword}
-  
-        onKeywordChange={onKeywordChange}
-  
-        onCreate={onCreate}
-  
-        createLabel="Cost Code"
-  
-      />
-  
-    );
-  
-  }
+  MasterToolbar,
+} from "@/components/master-data";
+
+interface Props {
+
+  keyword: string;
+
+  onKeywordChange(
+    value: string
+  ): void;
+
+  onCreate(): void;
+
+  onImport?(): void;
+
+  onExportExcel?(): void;
+
+  onExportCsv?(): void;
+
+  onDownloadTemplate?(): void;
+
+}
+
+export default function CostCodeToolbar({
+
+  keyword,
+
+  onKeywordChange,
+
+  onCreate,
+
+  onImport,
+
+  onExportExcel,
+
+  onExportCsv,
+
+  onDownloadTemplate,
+
+}: Props) {
+
+  return (
+
+    <MasterToolbar
+
+      keyword={keyword}
+
+      onKeywordChange={onKeywordChange}
+
+      createLabel="Cost Code"
+
+      onCreate={onCreate}
+
+      onImport={onImport}
+
+      onExport={onExportExcel}
+
+      onExportCsv={onExportCsv}
+
+      onDownloadTemplate={onDownloadTemplate}
+
+    />
+
+  );
+
+}
